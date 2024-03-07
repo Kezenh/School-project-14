c12c4d1af7bd2955f59f1775bb0397fd24335ef3
Kezenh <quentin425@hotmail.fr>
2024-03-07 18:20:23 +0100

    Titi

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ root.render(
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/employee-list" element={<EmployeeList />} />
-        <Route path ="*" element={<Error />} />
+        <Route path ="*" element={<Home />} />
       </Routes>
     </Router>
   </Provider>
